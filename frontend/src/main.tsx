7bc8edee0daf81c819ff59578673ef7b05a8c149
Josh Kimmell <jkimmell@gmail.com>
2025-06-17 09:27:35 -0500

    removed .html from assets included in vite-config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,14 @@
-// import { SessionContextProvider } from '@supabase/auth-helpers-react';
-// import supabase from '@lib/supabase.ts';
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import '@components/components.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-  // </React.StrictMode> 
+  </React.StrictMode> 
 );
